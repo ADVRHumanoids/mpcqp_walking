31050da4684a1e3898606203a8ea207bf485b187
Enrico Mingo <enrico.mingo@iit.it>
2017-10-12 10:17:39 +0200

    Added header with default defines

---
+++ include/mpcqp_walking/defines.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (C) 2017 cogimon
+  Author:  Enrico Mingo Hoffman
+  email: enrico.mingo@iit.it
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef _DEFINES_MPC_H__
+#define _DEFINES_MPC_H__
+
+#define DEFAULT_SINGLE_SUPPORT_KNOT_NUM  15
+#define DEFAULT_DOUBLE_SUPPORT_KNOT_NUM  1
+#define DEFAULT_PREVIEW_WALKING_STEP  3
+#define DEFAULT_STEP_TIME 0.1
+#define DEFAULT_JERK_WEIGHT  1e-3
+#define DEFAULT_VELOCITY_WEIGHT  1e+1 // 1e+1
+#define DEFAULT_ZMP_WEIGHT  1e+3
+#define DEFAULT_GRAVITY  9.81
+
+
+#endif
